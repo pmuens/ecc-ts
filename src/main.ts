608adc6ddcf50f7bc5335b4105d3f2a82e8bd43e
Philipp Muens <philipp@muens.io>
2024-04-04 14:57:47 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-export default interface Person {
-  firstName: string;
-  lastName: string;
+import { ECDH } from "./ecdh.ts";
+import { int2Hex } from "./utils.ts";
+
+const { log } = console;
+
+function main() {
+  const ecdhAlice = new ECDH();
+  const ecdhBob = new ECDH();
+
+  const sharedSecretAlice = ecdhAlice.deriveSecret(ecdhBob.pk);
+  const sharedSecretBob = ecdhBob.deriveSecret(ecdhAlice.pk);
+
+  const aliceX = int2Hex(sharedSecretAlice.x).slice(0, 10);
+  const aliceY = int2Hex(sharedSecretAlice.y).slice(0, 10);
+
+  const bobX = int2Hex(sharedSecretBob.x).slice(0, 10);
+  const bobY = int2Hex(sharedSecretBob.y).slice(0, 10);
+
+  log(`Shared Secret (Alice): (${aliceX}..., ${aliceY}...)`);
+  log(`Shared Secret (Bob): (${bobX}..., ${bobY}...)`);
 }
 
-export function sayHello(p: Person): string {
-  return `Hello, ${p.firstName}!`;
-}
+main();
